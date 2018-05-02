94dfdc4f714c1e11eadca66254bd5aba152336ac
koushal <koushal.ogirala@gmail.com>
2018-05-02 13:27:54 -0400

    stats avg and min, max

--- code/analysis.py
+++ code/analysis.py
@@ -24,3 +24,20 @@ for year in years:
 
     # Handle abnomalities and missing data
     clean_df = df.filter(df['q_flag'] == '')
+
+    print("\nYear %s Stats:\n" % year)
+    # 1. Average min
+    res = clean_df.filter(clean_df['ele'] == 'TMIN').groupby().avg('val').first()
+    print('Avg. Min Temp = %.2f degrees Celsius' % (res['avg(val)'] / 10.0))
+
+    # 1. Average max
+    res = clean_df.filter(clean_df['ele'] == 'TMAX').groupby().avg('val').first()
+    print('Avg. Max Temp = %.2f degrees Celsius' % (res['avg(val)'] / 10.0))
+
+    # 2. Max TMAX
+    res = clean_df.filter(clean_df['ele'] == 'TMAX').groupby().max('val').first()
+    print('Max TMAX value = %.2f degrees Celsius' % (res['max(val)'] / 10.0))
+
+    # 2. Min TMIN
+    res = clean_df.filter(clean_df['ele'] == 'TMIN').groupby().min('val').first()
+    print('Min TMIN value = %.2f degrees Celsius' % (res['min(val)'] / 10.0))
